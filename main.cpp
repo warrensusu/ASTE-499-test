c8d3d5e0fee69a260fcf0a130b5202cf4444cd7d
Warren Su <warrensu@usc.edu>
2021-03-10 04:19:19 +0800

    Second commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Vec.h"
 
 int main(int num_args, char **args) {
-    double3 a{0,1,2}; //! null constructor
+    double3 a{0,1,2};
     double3 b{0,0,1};
     std::cout<<a-b<<std::endl;
     std::cout<<dot(a,b)<<std::endl;
